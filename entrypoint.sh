c63fd31f19c72789327257d3c510a03db40d582e
ywfwj2008 <ywfwj2008@163.com>
2017-06-26 01:46:28 +0800

    clear iplist.txt

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+echo "" > /www/server/panel/data/iplist.txt
+
 /etc/init.d/bt start
 
 exec "$@"
--- php/entrypoint.sh
+++ php/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+echo "" > /www/server/panel/data/iplist.txt
+
 /etc/init.d/bt start
 /etc/init.d/php-fpm-56 start
 /etc/init.d/php-fpm-70 start
