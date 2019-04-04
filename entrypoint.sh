cfcd48070da29dbf5d6f47780bacb50d56cf1c3e
ywfwj2008 <ywfwj2008@163.com>
2019-04-05 00:35:46 +0800

    优化启动文件

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,20 +5,49 @@ export PATH
 
 # reset memory
 Mem=`free -m | awk '/Mem:/{print $2}'`
-if [ "$Mem" -le 640 ];then
-    MEMORY_LIMIT=64
-elif [ "$Mem" -gt 640 -a "$Mem" -le 1280 ];then
-    MEMORY_LIMIT=128
-elif [ "$Mem" -gt 1280 -a "$Mem" -le 2500 ];then
-    MEMORY_LIMIT=192
-elif [ "$Mem" -gt 2500 -a "$Mem" -le 3500 ];then
-    MEMORY_LIMIT=256
-elif [ "$Mem" -gt 3500 -a "$Mem" -le 4500 ];then
-    MEMORY_LIMIT=320
-elif [ "$Mem" -gt 4500 -a "$Mem" -le 8000 ];then
-    MEMORY_LIMIT=384
-elif [ "$Mem" -gt 8000 ];then
-    MEMORY_LIMIT=448
+if [ $Mem -le 640 ]; then
+  Memory_limit=64
+elif [ $Mem -gt 640 -a $Mem -le 1280 ]; then
+  Memory_limit=128
+elif [ $Mem -gt 1280 -a $Mem -le 2500 ]; then
+  Memory_limit=192
+elif [ $Mem -gt 2500 -a $Mem -le 3500 ]; then
+  Memory_limit=256
+elif [ $Mem -gt 3500 -a $Mem -le 4500 ]; then
+  Memory_limit=320
+elif [ $Mem -gt 4500 -a $Mem -le 8000 ]; then
+  Memory_limit=384
+elif [ $Mem -gt 8000 ]; then
+  Memory_limit=448
+fi
+
+sed -i "s@^memory_limit.*@memory_limit = ${Memory_limit}M@" ${PHP_70_PATH}/etc/php.ini
+sed -i "s@^opcache.memory_consumption.*@opcache.memory_consumption=${Memory_limit}@" ${PHP_70_PATH}/etc/php.ini
+if [ $Mem -le 3000 ]; then
+  sed -i "s@^pm.max_children.*@pm.max_children = $(($Mem/3/20))@" ${PHP_70_PATH}/etc/php-fpm.conf
+  sed -i "s@^pm.start_servers.*@pm.start_servers = $(($Mem/3/30))@" ${PHP_70_PATH}/etc/php-fpm.conf
+  sed -i "s@^pm.min_spare_servers.*@pm.min_spare_servers = $(($Mem/3/40))@" ${PHP_70_PATH}/etc/php-fpm.conf
+  sed -i "s@^pm.max_spare_servers.*@pm.max_spare_servers = $(($Mem/3/20))@" ${PHP_70_PATH}/etc/php-fpm.conf
+elif [ $Mem -gt 3000 -a $Mem -le 4500 ]; then
+  sed -i "s@^pm.max_children.*@pm.max_children = 50@" ${PHP_70_PATH}/etc/php-fpm.conf
+  sed -i "s@^pm.start_servers.*@pm.start_servers = 30@" ${PHP_70_PATH}/etc/php-fpm.conf
+  sed -i "s@^pm.min_spare_servers.*@pm.min_spare_servers = 20@" ${PHP_70_PATH}/etc/php-fpm.conf
+  sed -i "s@^pm.max_spare_servers.*@pm.max_spare_servers = 50@" ${PHP_70_PATH}/etc/php-fpm.conf
+elif [ $Mem -gt 4500 -a $Mem -le 6500 ]; then
+  sed -i "s@^pm.max_children.*@pm.max_children = 60@" ${PHP_70_PATH}/etc/php-fpm.conf
+  sed -i "s@^pm.start_servers.*@pm.start_servers = 40@" ${PHP_70_PATH}/etc/php-fpm.conf
+  sed -i "s@^pm.min_spare_servers.*@pm.min_spare_servers = 30@" ${PHP_70_PATH}/etc/php-fpm.conf
+  sed -i "s@^pm.max_spare_servers.*@pm.max_spare_servers = 60@" ${PHP_70_PATH}/etc/php-fpm.conf
+elif [ $Mem -gt 6500 -a $Mem -le 8500 ]; then
+  sed -i "s@^pm.max_children.*@pm.max_children = 70@" ${PHP_70_PATH}/etc/php-fpm.conf
+  sed -i "s@^pm.start_servers.*@pm.start_servers = 50@" ${PHP_70_PATH}/etc/php-fpm.conf
+  sed -i "s@^pm.min_spare_servers.*@pm.min_spare_servers = 40@" ${PHP_70_PATH}/etc/php-fpm.conf
+  sed -i "s@^pm.max_spare_servers.*@pm.max_spare_servers = 70@" ${PHP_70_PATH}/etc/php-fpm.conf
+elif [ $Mem -gt 8500 ]; then
+  sed -i "s@^pm.max_children.*@pm.max_children = 80@" ${PHP_70_PATH}/etc/php-fpm.conf
+  sed -i "s@^pm.start_servers.*@pm.start_servers = 60@" ${PHP_70_PATH}/etc/php-fpm.conf
+  sed -i "s@^pm.min_spare_servers.*@pm.min_spare_servers = 50@" ${PHP_70_PATH}/etc/php-fpm.conf
+  sed -i "s@^pm.max_spare_servers.*@pm.max_spare_servers = 80@" ${PHP_70_PATH}/etc/php-fpm.conf
 fi
 
 # start run application
